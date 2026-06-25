490f66995025cafb760e9416e1ffd72942d2e3a0
heexion <alsgml030901@naver.com>
2026-06-25 20:51:03 +0900

    CHORE: 서버 프로젝트 초기 설정

---
+++ ai-study-planner/src/test/java/com/example/aistudyplanner/AiStudyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aistudyplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiStudyPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
