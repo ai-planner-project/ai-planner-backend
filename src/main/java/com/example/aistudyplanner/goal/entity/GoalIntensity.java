7b84837ca8b389db068eae7189bc53393c8fb279
heexion <alsgml030901@naver.com>
2026-06-27 15:24:32 +0900

    FEAT: Goal 엔티티 추가

---
+++ src/main/java/com/example/aistudyplanner/goal/entity/GoalIntensity.java
@@ -0,0 +1,9 @@
+package com.example.aistudyplanner.goal.entity;
+
+public enum GoalIntensity {
+    EASY,
+    NORMAL,
+    FOCUSED,
+    HARD,
+    EXTREME
+}
---
+++ src/main/java/com/example/aistudyplanner/goal/entity/GoalStatus.java
@@ -0,0 +1,6 @@
+package com.example.aistudyplanner.goal.entity;
+
+public enum GoalStatus {
+    IN_PROGRESS,
+    COMPLETED
+}
